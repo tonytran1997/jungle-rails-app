2b87f435e90921ee1c475879de32c47e0eba584a
Tony Tran <nobackbone@Tonys-MacBook-Air.local>
2023-09-18 03:06:21 -0400

    Update admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   ENV['ADMIN_USERNAME'], password: 
   ENV['ADMIN_PASSWORD']
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
