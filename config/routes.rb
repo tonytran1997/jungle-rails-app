97c18011a1a50ff357722a8f969ff3de0fd624f1
Tony Tran <nobackbone@Tonys-MacBook-Air.local>
2023-09-18 03:24:06 -0400

    add feature About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
